d3e0866938d2e1741e2353d49a183bc7542cde7f
August2111 <Info@FlapsOnline.de>
2024-04-07 23:25:04 +0200

    [SkySight] netcdf.mk: use HAVE_SKYSIGHT
    netcdf-cxx4 instead of netcdf_c++4

--- build/netcdf.mk
+++ build/netcdf.mk
@@ -1,10 +1,15 @@
-NETCDF = y
+ifeq ($(HAVE_SKYSIGHT),y)
+  NETCDF = y
 
-ifneq ($(TARGET),ANDROID)
-$(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
-$(eval $(call link-library,netcdfcpp,NETCDF))
-NETCDF_LDLIBS = -lnetcdf_c++4 -lnetcdf
+  ifeq ($(TARGET),ANDROID)
+    NETCDF_LDLIBS += -l:libnetcdf_c++.a -l:libnetcdf.a
+  else
+    $(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
+    $(eval $(call link-library,netcdfcpp,NETCDF))
+    NETCDF_LDLIBS = -lnetcdf_c++4 -lnetcdf
+  endif
+  LDLIBS += $(NETCDF_LDLIBS)
+
 else
-NETCDF_LDLIBS += -l:libnetcdf_c++.a -l:libnetcdf.a
+  NETCDF = n
 endif
-LDLIBS += $(NETCDF_LDLIBS)
