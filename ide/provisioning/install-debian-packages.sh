42d33656cbe0bbf336d22ae2569aefaf6a6e463f
August2111 <Info@FlapsOnline.de>
2024-04-08 11:30:36 +0200

    [SkySight] install-debian-packages.sh - load necessary libs for linux

--- ide/provisioning/install-debian-packages.sh
+++ ide/provisioning/install-debian-packages.sh
@@ -70,8 +70,11 @@ install_linux() {
     imagemagick gettext \
     mesa-common-dev libgl1-mesa-dev libegl1-mesa-dev \
     fonts-dejavu \
-    xz-utils
-  echo
+    xz-utils \
+    netcdf-bin \
+    libnetcdf-c++4-dev \
+    libnetcdf-dev
+echo
 }
 
 install_wayland() {
