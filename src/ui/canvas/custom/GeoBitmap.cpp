05e6ba03625b71e92a87bf144d64813081d12801
August2111 <Info@FlapsOnline.de>
2024-09-09 15:34:11 +0200

    [SkySight, Windows] - canvas/custom/GeoBitmap.cpp - remove tchar.h, use path  instead of result.first use the flag GDI_WITH_TESTSAVE for Testing the downloaded GeoBitmaps add a return value - and remove warning - is obsolete now
    # Conflicts:
    #	src/ui/canvas/custom/GeoBitmap.cpp

--- src/ui/canvas/custom/GeoBitmap.cpp
+++ src/ui/canvas/custom/GeoBitmap.cpp
@@ -6,33 +6,34 @@
 #include "Geo/Quadrilateral.hpp"
 #include "system/Path.hpp"
 
+#include "LogFile.hpp"
+
 #ifdef USE_GEOTIFF
-#include "LibTiff.hpp"
+# include "LibTiff.hpp"
 #endif
 
 #include <stdexcept>
-
-#include <tchar.h>
-
-#if !defined(USE_GEOTIFF) && defined(__GNUC__)
-#pragma GCC diagnostic ignored "-Wsuggest-attribute=noreturn"
-#endif
 
 GeoQuadrilateral
 Bitmap::LoadGeoFile([[maybe_unused]] Path path)
 {
 #ifdef USE_GEOTIFF
+  LogFmt("Bitmap::LoadGeoFile: {}", path.c_str());
+  LogFmt("Bitmap::LoadGeoFile: USE_GEOTIFF");
   if (path.EndsWithIgnoreCase(_T(".tif")) ||
       path.EndsWithIgnoreCase(_T(".tiff"))) {
     auto result = LoadGeoTiff(path);
-    if (!Load(std::move(result.first)))
+    if (!LoadFile(path))
       throw std::runtime_error("Failed to use geo image file");
 
     assert(IsDefined());
 
     return result.second;
   }
-#endif
-
   throw std::runtime_error("Unsupported geo image file");
+#else  // USE_GEOTIFF
+  LogFmt("Bitmap::LoadGeoFile: {}", path.c_str());
+#endif  // USE_GEOTIFF
+  return {};
+
 }
