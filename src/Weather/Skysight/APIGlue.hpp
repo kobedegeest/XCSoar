a37dd7c67a658d0f750abf002261559eeb5cc05a
August2111 <Info@FlapsOnline.de>
2024-09-15 17:15:17 +0200

    [SkySight] APIGlue.hpp - remove flag 'to_file': this is done with path.empty()

--- src/Weather/Skysight/APIGlue.hpp
+++ src/Weather/Skysight/APIGlue.hpp
@@ -28,7 +28,6 @@ enum class SkysightCallType {
 struct SkysightRequestArgs {
   const std::string url;
   const std::string path;
-  // const bool to_file;  // August2111: if path is not empty, to_file is true!
   const SkysightCallType calltype;
   const std::string region;
   const std::string layer;
@@ -36,12 +35,10 @@ struct SkysightRequestArgs {
   const uint64_t to;
   const SkysightCallback cb;
   SkysightRequestArgs(const std::string_view _url, const std::string_view _path,
-//		      const bool _to_file, const SkysightCallType _ct,
 		      const SkysightCallType _ct,
 		      const std::string _region, const std::string _layer,
 		      const uint64_t _from = 0, const uint64_t _to = 0,
 		      const SkysightCallback _cb = nullptr):
-//    url(_url), path(_path), to_file(_to_file), calltype(_ct),
     url(_url), path(_path), calltype(_ct),
     region(_region), layer(_layer), from(_from), to(_to), cb(_cb) {};
 };
