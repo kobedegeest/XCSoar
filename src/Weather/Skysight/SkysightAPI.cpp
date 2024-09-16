d239c684eb56c576566bfb5c43b777466b6e64e4
August2111 <Info@FlapsOnline.de>
2024-09-16 23:20:04 +0200

    [SkySight] SkysightAPI.cpp - change filename for data details with forecast time
    # Conflicts:
    #	src/Weather/Skysight/SkysightAPI.cpp

--- src/Weather/Skysight/SkysightAPI.cpp
+++ src/Weather/Skysight/SkysightAPI.cpp
@@ -144,10 +144,10 @@ SkysightAPI::GetPath(SkysightCallType type, const char *const layer,
     break;
   case SkysightCallType::DataDetails:
     fc = FromUnixTime(fctime);
-    filename.Format("%s-datafiles-%s-%04d%02d%02d%02d%02d.json",
-        region.c_str(), layer, fc.year, fc.month, fc.day,
-        fc.hour, fc.minute);
-      break;
+    filename.Format("%s-datafiles-%s-%02d-%02d%02d.json",
+                    region.c_str(), layer,
+                    fc.day, fc.hour, fc.minute);
+    break;
   case SkysightCallType::Data:
     fc = FromUnixTime(fctime);
     filename.Format("%s-%s-%04d%02d%02d%02d%02d.nc", region.c_str(), layer, 
