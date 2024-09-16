79fc7aab24189f97de7c1a26e7986c317498e538
August2111 <Info@FlapsOnline.de>
2024-09-16 17:53:07 +0200

    [Skysight] Skysight.cpp and hpp - deleting the API if starting the SkySight... ...twice (or more) - possible at changing the metric
    For this api has to be set to a predefined value (= nullptr)

--- src/Weather/Skysight/Skysight.cpp
+++ src/Weather/Skysight/Skysight.cpp
@@ -26,19 +26,16 @@
 #include <vector>
 #include <memory>
 #include <ctime>
-
-// #if defined(_DEBUG) && defined(WIN_SKYSIGHT)
-#define SKYSIGHT_DEBUG  1
 #include <chrono>
+
 #if defined(SKYSIGHT_DEBUG)
 # include <filesystem>
 # include <regex>
 # include <iomanip>  // put_time
 # include <thread>
 # include <fmt/format.h>
-# ifdef __MSVC__
+# ifdef USE_STD_FORMAT
 #   include <format>
-#   define USE_STD_FORMAT 1
 # endif
 #endif
 
@@ -317,6 +314,12 @@ Skysight::Skysight(CurlGlobal &_curl)
 void
 Skysight::Init()
 {
+  if (api) {
+    delete api;
+    api = nullptr;
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+  }
+
   const auto settings = CommonInterface::GetComputerSettings().weather.skysight;
   region = settings.region.c_str();
   email = settings.email.c_str();
