fb248de02c6823992bf9e77c05e735745a233071
August2111 <Info@FlapsOnline.de>
2024-09-24 19:30:17 +0200

    [SkySight] Layers.hpp - create only one layer object (no difference between... selected and active layer

--- src/Weather/Skysight/Layers.hpp
+++ src/Weather/Skysight/Layers.hpp
@@ -19,22 +19,53 @@ struct SkysightLayer {
   uint64_t last_update = 0;
   std::map<float, LegendColor> legend;
 
+  double from = 0;
+  double to = 0;
+  double mtime = 0;
+  bool updating = false;
+
+  BrokenDateTime forecast_time = BrokenDateTime(0, 0, 0);
 public:
   SkysightLayer(std::string _id, std::string _name, std::string _desc):
     id(_id), name(_name), desc(_desc) {}
-  SkysightLayer(const SkysightLayer &m):
-    id(m.id), name(m.name), desc(m.desc), last_update(m.last_update),
-    legend(m.legend) {}
+
+  SkysightLayer(std::string_view _id):
+    id(_id), name(""), desc("") {}
+
+#if 1
+  SkysightLayer(const SkysightLayer &layer):
+    id(layer.id), name(layer.name), desc(layer.desc), last_update(layer.last_update),
+    legend(layer.legend),
+    from(layer.from), to(layer.to), mtime(layer.mtime),
+    updating(layer.updating) {}
+
+  const SkysightLayer &operator =(const SkysightLayer &layer) { return layer; }
+#endif
+
   bool operator==(std::string_view _id) {
     if (_id.empty()) return false;
 
     return (id == _id);
   };
+
+  bool operator < (const BrokenDateTime &t) {
+    if (!forecast_time.IsPlausible())
+      return false;
+
+    return (forecast_time.ToTimePoint() < t.ToTimePoint());
+  }
+
 };
 
+
+#if 0
 /*
  * Skysight chart which is overlaid
  */
+#if 1
+#define SkysightActiveLayer SkysightLayer
+#define DisplayedLayer SkysightLayer
+#else 
 struct SkysightActiveLayer {
   SkysightLayer *layer;
   double from = 0;
@@ -58,7 +89,6 @@ public:
   };
 
 };
-
 struct DisplayedLayer {
   SkysightLayer *layer;
   BrokenDateTime forecast_time;
@@ -84,3 +114,5 @@ struct DisplayedLayer {
     return (forecast_time.ToTimePoint() < t.ToTimePoint());
   }
 };
+#endif
+#endif
