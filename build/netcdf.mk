d4e7d680ebdf98c90f641dda2c4efbc34cfe4cd5
August2111 <Info@FlapsOnline.de>
2025-02-06 22:53:41 +0100

    [SkySight, 3rd party, build] netcdf.mk - netcdf on MinGW (failure at including  lib netcdf at MinGW (=> Windows)

--- build/netcdf.mk
+++ build/netcdf.mk
@@ -4,9 +4,15 @@ ifeq ($(HAVE_SKYSIGHT),y)
   ifeq ($(TARGET),ANDROID)
     NETCDF_LDLIBS += -l:libnetcdf_c++.a -l:libnetcdf.a
   else
-    $(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
-    $(eval $(call link-library,netcdfcpp,NETCDF))
-    NETCDF_LDLIBS = -lnetcdf_c++4 -lnetcdf
+    ifeq ($(HAVE_WIN32),y)
+      $(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
+      $(eval $(call link-library,netcdfcpp,NETCDF))
+      NETCDF_LDLIBS = -lnetcdf_c++ -lnetcdf
+    else
+      $(eval $(call pkg-config-library,NETCDF,netcdf-cxx4))
+      $(eval $(call link-library,netcdfcpp,NETCDF))
+      NETCDF_LDLIBS = -lnetcdf_c++4 -lnetcdf
+    endif
   endif
   LDLIBS += $(NETCDF_LDLIBS)
 
