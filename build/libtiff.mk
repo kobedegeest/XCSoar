9cf73964f0e9ae6a87693d3a7bb29dadea3f2d36
August2111 <Info@FlapsOnline.de>
2025-02-11 21:38:32 +0100

    [SkySight, build] libtiff.mk - move (skysight) 3rd party libs to LDLIBS

--- build/libtiff.mk
+++ build/libtiff.mk
@@ -1,31 +1,25 @@
-# Experimental feature - only enabled on Linux for now
-ifeq ($(TARGET)$(TARGET_IS_KOBO),UNIXn)
-GEOTIFF ?= y
-else ifeq ($(TARGET),ANDROID)
-GEOTIFF ?= y
+ifeq ($(HAVE_SKYSIGHT),y)
+    GEOTIFF ?= y
+
+    $(eval $(call pkg-config-library,LIBTIFF,libtiff-4))
+    LIBTIFF_CPPFLAGS += -DUSE_LIBTIFF
+  
+    ifeq ($(GEOTIFF),y)
+      LIBTIFF_CPPFLAGS += -DUSE_GEOTIFF
+      ifneq ($(USE_THIRDPARTY_LIBS),y)
+        LIBTIFF_CPPFLAGS += -isystem /usr/include/geotiff
+      endif
+      LIBTIFF_LDLIBS += -ltiff -lgeotiff
+    endif
+  
+    ifeq ($(GEOTIFF)$(USE_THIRDPARTY_LIBS),yy)
+      $(eval $(call pkg-config-library,PROJ,proj))
+      LIBTIFF_CPPFLAGS += $(PROJ_CPPFLAGS)
+      LIBTIFF_LDLIBS += $(PROJ_LDLIBS) -lsqlite3 -lproj
+    endif
 else
-GEOTIFF ?= n
+  GEOTIFF ?= n
 endif
 
+LDLIBS += $(LIBTIFF_LDLIBS)
 TIFF ?= $(GEOTIFF)
-
-ifeq ($(TIFF),y)
-
-$(eval $(call pkg-config-library,LIBTIFF,libtiff-4))
-LIBTIFF_CPPFLAGS += -DUSE_LIBTIFF
-
-ifeq ($(GEOTIFF),y)
-LIBTIFF_CPPFLAGS += -DUSE_GEOTIFF
-ifneq ($(USE_THIRDPARTY_LIBS),y)
-LIBTIFF_CPPFLAGS += -isystem /usr/include/geotiff
-endif
-LIBTIFF_LDLIBS += -lgeotiff
-endif
-
-ifeq ($(GEOTIFF)$(USE_THIRDPARTY_LIBS),yy)
-$(eval $(call pkg-config-library,PROJ,proj))
-LIBTIFF_CPPFLAGS += $(PROJ_CPPFLAGS)
-LIBTIFF_LDLIBS += $(PROJ_LDLIBS)
-endif
-
-endif
