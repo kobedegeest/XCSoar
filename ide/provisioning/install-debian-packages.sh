a2b2ce1b725488aad720b523943b8e08f9a41ead
August2111 <Info@FlapsOnline.de>
2025-03-26 22:19:19 +0100

    ide/provisioning/install-debian-packages.sh - use netcdf  on Linux as static

--- ide/provisioning/install-debian-packages.sh
+++ ide/provisioning/install-debian-packages.sh
@@ -71,9 +71,11 @@ install_linux() {
     mesa-common-dev libgl1-mesa-dev libegl1-mesa-dev \
     fonts-dejavu \
     xz-utils \
-    netcdf-bin \
-    libnetcdf-c++4-dev \
-    libnetcdf-dev
+    
+    # not longer installed...
+    # apt-get install --assume-yes --no-install-recommends netcdf-bin libnetcdf-c++4-dev  libnetcdf-dev
+    # apt-get remove -y netcdf-bin libnetcdf-c++4-dev  libnetcdf-dev
+    
 echo
 }
 
