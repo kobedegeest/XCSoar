August2111/OpenSoar

[SkySight] MapWindow/GlueMapWindowOverlays.cpp - show real layer on map

src/MapWindow/GlueMapWindowOverlays.cpp
@@ -347,7 +347,8 @@ GlueMapWindow::DrawMapScale(Canvas &canvas, const PixelRect &rc,
       buffer += gettext(label);
     }
   } else if (skysight) {
-    buffer += "SkySight: Layer";
+    buffer += "SkySight: ";
+    buffer += skysight->GetActiveLayerName();
   }
 
   if (!buffer.empty()) {

kobedegeest/XCSoar

src/MapWindow/GlueMapWindowOverlays.cpp
@@ -346,7 +346,8 @@ GlueMapWindow::DrawMapScale(Canvas &canvas, const PixelRect &rc,
       buffer += gettext(label);
     }
   } else if (skysight) {
-    buffer += "SkySight: Layer";
+    buffer += "SkySight: ";
+    buffer += skysight->GetDisplayedLayerName();
   }
 
   if (!buffer.empty()) {